f7609a2df986eb9299500735eecdeec8bbf749c8
Connor G Porell <cporell@mitre.org>
2024-05-17 16:55:16 -0400

    Fix some hidden merge conflict markers

--- search-website-functions-v4/api/readme.md
+++ search-website-functions-v4/api/readme.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 function test
-=======
-function test
->>>>>>> 5814cc0ecd37bef1db2f64b540f78dd76ef50d42
--- search-website-functions-v4/api/src/lib/config.js
+++ search-website-functions-v4/api/src/lib/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const CONFIG = {
     SearchIndexName: process.env["SearchIndexName"] || "good-books",
     SearchApiQueryKey: process.env["SearchApiKey"] || "",
@@ -9,15 +8,3 @@ console.log(CONFIG);
 if (!CONFIG.SearchIndexName || !CONFIG.SearchApiQueryKey || !CONFIG.SearchServiceName) throw Error("./config.js::Cognitive Services key is missing");
 
 module.exports = { CONFIG };
-=======
-const CONFIG = {
-    SearchIndexName: process.env["SearchIndexName"] || "good-books",
-    SearchApiQueryKey: process.env["SearchApiKey"] || "",
-    SearchServiceName: process.env["SearchServiceName"] || "",
-    SearchFacets: process.env["SearchFacets"] || "authors*,language_code", 
-}
-console.log(CONFIG);
-if (!CONFIG.SearchIndexName || !CONFIG.SearchApiQueryKey || !CONFIG.SearchServiceName) throw Error("./config.js::Cognitive Services key is missing");
-
-module.exports = { CONFIG };
->>>>>>> 5814cc0ecd37bef1db2f64b540f78dd76ef50d42
